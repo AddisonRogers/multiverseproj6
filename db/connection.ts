5216fda60430468db5f5158f96f63d49c8c11c08
Addison Rogers <addisonrogers@fsp.co>
2024-05-24 02:12:49 +0100

    deleting stuff

--- db/connection.ts
+++
@@ -1,5 +0,0 @@
-import { drizzle } from 'drizzle-orm/bun-sqlite';
-import { Database } from 'bun:sqlite';
-
-const sqlite = new Database(':memory:');
-export const db = drizzle(sqlite);
--- models/Show.ts
+++
@@ -1,9 +0,0 @@
-// import our db, Model, DataTypes
-import {integer, sqliteTable, text} from "drizzle-orm/sqlite-core";
-
-export const Shows = sqliteTable('shows', {
-  title: text('title'),
-  genre: text('genre'),
-  rating: integer('rating'),
-  available: integer('available', { mode: 'boolean'}),
-  });
